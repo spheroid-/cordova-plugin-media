06eaf0020d56ea942c43513f3551c8e7d137182d
ghenry22 <ghenry22@gmail.com>
2015-09-07 10:39:12 +0800

    test

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -384,13 +384,9 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
         while(testDuration < 1){
             testDuration = this.player.getDuration();
             Log.d(LOG_TAG, "AudioPlayer.getDuration(" + testDuration + ")");
-
-            if (testDuration > 1){
-                return (testDuration / 1000.0f);
-            }
         }
 
-        // return (this.player.getDuration() / 1000.0f);
+         return (testDuration / 1000.0f);
     }
 
     /**
